0b8fcb801557d7a2b164751802c299a9feb657c5
vladimir kazimir <kazimir.vm@gmail.com>
2023-03-09 14:54:08 +0300

    Task №3

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pprint import pprint
 
+import os
+
 with open('cook_book.txt', 'r', encoding='utf-8') as file:
     cook_book = {}
     for i in file:
@@ -27,3 +29,23 @@ def get_shop_list_by_dishes(dishes, person_count):
         else:
             dict_all_ingredients[_['ingredient_name']]['quantity'] += int(_['quantity']) * person_count
     return pprint(dict_all_ingredients)
+
+
+list_file_address = ['1.txt', '2.txt', '3.txt']
+list_text_all_files = []
+for _ in range(len(list_file_address)):
+    full_path = os.path.join(os.getcwd(), 'a_group_of_files_to_merge', list_file_address[_])
+    with open(full_path, 'r', encoding='utf-8') as file:
+        list_text_all_files.append(file.readlines())
+list_text_all_files.sort()
+list_text_all_files.reverse()
+
+
+with open('sorted_file.txt', 'w', encoding='utf-8') as file:
+    for i, _ in enumerate(list_text_all_files):
+        file.write(list_file_address[i])
+        file.write('\n')
+        file.write(str(len(list_text_all_files[i])))
+        file.write('\n')
+        file.writelines(_)
+        file.write('\n')
